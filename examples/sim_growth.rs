67675e0c313ff92452a5177bfb8107a9cfb7c48f
Polgár András <azazdeaz@gmail.com>
2022-07-01 16:28:51 +0200

    Implement fixed step growing

--- examples/sim_growth.rs
+++ examples/sim_growth.rs
@@ -1,5 +1,6 @@
 use bevy::{
     app::ScheduleRunnerSettings,
+    ecs::schedule::ShouldRun,
     prelude::*,
     render::mesh::{
         skinning::{SkinnedMesh, SkinnedMeshInverseBindposes},
@@ -20,14 +21,53 @@ fn main() {
         .insert_resource(ScheduleRunnerSettings {
             run_mode: bevy::app::RunMode::Loop { wait: None },
         })
+        .insert_resource(GrowSteps::from_steps(6))
         .add_plugins(DefaultPlugins)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugin(RapierDebugRenderPlugin::default())
         .add_startup_system(setup_plant)
         .add_startup_system(setup_scene)
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(run_if_growing)
+                .with_system(count_grow_steps)
+                // .with_system(grow)
+                // .with_system(extend),
+        )
         .run();
 }
 
+struct GrowSteps {
+    current: u32,
+    max: u32,
+}
+impl GrowSteps {
+    fn from_steps(max: u32) -> Self {
+        Self {
+            current: 0,
+            max: max,
+        }
+    }
+    fn step(&mut self) {
+        self.current += 1;
+    }
+    fn is_done(&self) -> bool {
+        self.max <= self.current
+    }
+}
+
+fn run_if_growing(grow_steps: Res<GrowSteps>) -> ShouldRun {
+    if grow_steps.is_done() {
+        ShouldRun::No
+    } else {
+        ShouldRun::Yes
+    }
+}
+
+fn count_grow_steps(mut grow_steps: ResMut<GrowSteps>) {
+    println!("STEP: {}", grow_steps.current);
+    grow_steps.step();
+}
 
 #[derive(Component, Debug, Clone)]
 struct Stem {
@@ -50,8 +90,8 @@ impl Default for Stem {
     }
 }
 
-fn grow(query: Query<(&Parent, &mut Stem)>, parent_stem: Query<&Stem>) {
-    for (parent, stem) in query.iter_mut() {
+fn grow(mut query: Query<(&Parent, &mut Stem)>, parent_stem: Query<&Stem>) {
+    for (parent, mut stem) in query.iter_mut() {
         stem.length += (stem.max_length - stem.length) / 3.0;
         let parent = parent_stem.get(parent.0);
         let prev_radius = if let Ok(parent) = parent {
@@ -65,20 +105,23 @@ fn grow(query: Query<(&Parent, &mut Stem)>, parent_stem: Query<&Stem>) {
 
 fn extend(
     mut commands: Commands,
-    query: Query<(Entity, &mut Stem, &Children)>,
+    mut query: Query<(Entity, &mut Stem, &Children)>,
     child_stem: Query<&Stem>,
 ) {
-    for (stem_entity, stem, children) in query.iter_mut() {
+    for (stem_entity, mut stem, children) in query.iter_mut() {
         let has_following = children.iter().any(|c| child_stem.get(*c).is_ok());
         if !has_following {}
         if stem.length > stem.max_length * 0.8 && !has_following {
-            let next = commands.spawn().insert(Stem {
-                order: stem.order,
-                length: 0.2,
-                max_length: 4.0,
-                radius: 0.001,
-                direction: Quat::default(),
-            }).id();
+            let next = commands
+                .spawn()
+                .insert(Stem {
+                    order: stem.order,
+                    length: 0.2,
+                    max_length: 4.0,
+                    radius: 0.001,
+                    direction: Quat::default(),
+                })
+                .id();
             commands.entity(stem_entity).push_children(&[next]);
         }
     }
