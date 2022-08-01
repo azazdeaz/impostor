138aec106f89c4d37ed236b11a1486d4d2e6f2e9
Polgár András <azazdeaz@gmail.com>
2022-08-01 18:55:20 +0200

    Update scene from file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use core::panic;
 use std::io::Write;
 
-use bevy::{prelude::*, reflect::TypeRegistry, utils::Duration, render::render_resource::{Extent3d, TextureDimension, TextureFormat}};
+use bevy::{
+    ecs::{archetype::Archetypes, component::Components, entity::Entities},
+    prelude::*,
+    reflect::TypeRegistry,
+    render::render_resource::{Extent3d, TextureDimension, TextureFormat},
+    utils::Duration,
+};
 // use bevy_inspector_egui::{Inspectable, InspectorPlugin, WorldInspectorPlugin};
 
 fn main() {
@@ -21,10 +27,10 @@ fn main() {
         // .add_startup_system_to_stage(SAVE, save_scene_system.exclusive_system().at_end())
         .add_startup_system(load_scene_system)
         .add_system(update_primitives)
-        .add_system(rotate)
+        // .add_system(rotate)
+        .add_system(inspect)
         .run();
 }
-
 
 #[derive(Component, Reflect, Default)]
 #[reflect(Component)] // this tells the reflect derive to also reflect component behaviors
@@ -57,7 +63,6 @@ impl FromWorld for ComponentB {
     }
 }
 
-
 fn load_scene_system(mut commands: Commands, asset_server: Res<AssetServer>) {
     // "Spawning" a scene bundle creates a new entity and spawns new instances
     // of the given scene's entities as children of that entity.
@@ -77,14 +82,17 @@ fn save_scene_system(world: &mut World) {
     // use the current World.
     // let mut scene_world = World::new();
     // scene_world.spawn().insert(Primitive { shape: "cube".into() }).insert(Transform::identity());
-    
+
     // Scenes can be created from any ECS World. You can either create a new one for the scene or
     // use the current World.
     let mut scene_world = World::new();
 
-    scene_world
-        .spawn()
-        .insert_bundle((Primitive { shape: "cube".into() }, Transform::default()));
+    scene_world.spawn().insert_bundle((
+        Primitive {
+            shape: "cube".into(),
+        },
+        Transform::default(),
+    ));
 
     // The TypeRegistry resource contains information about all registered types (including
     // components). This is used to construct scenes.
@@ -105,22 +113,14 @@ fn save_scene_system(world: &mut World) {
     info!("{}", scene.serialize_ron(&type_registry).unwrap());
 
     let mut file = std::fs::File::create("assets/scenes/start_scene.scn.ron").unwrap();
-    file.write_all(
-        scene
-            .serialize_ron(&type_registry)
-            .unwrap()
-            .as_bytes(),
-    )
-    .unwrap();
+    file.write_all(scene.serialize_ron(&type_registry).unwrap().as_bytes())
+        .unwrap();
 }
-
 
 /// A marker component for our shapes so we can query them separately from the ground plane
 #[derive(Component, Reflect, Default)]
-#[reflect(Component)] 
+#[reflect(Component)]
 struct Shape;
-
-const X_EXTENT: f32 = 14.;
 
 fn setup(
     mut commands: Commands,
@@ -128,12 +128,13 @@ fn setup(
     mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-
     let debug_material = materials.add(StandardMaterial {
         base_color_texture: Some(images.add(uv_debug_texture())),
         ..default()
     });
-    commands.spawn().insert(DebugMaterial { handle: debug_material });
+    commands.spawn().insert(DebugMaterial {
+        handle: debug_material,
+    });
 
     // let shapes = [
     //     meshes.add(shape::Cube::default().into()),
@@ -191,14 +192,21 @@ fn setup(
 
 #[derive(Component)]
 struct DebugMaterial {
-    handle: Handle<StandardMaterial>
+    handle: Handle<StandardMaterial>,
 }
-
-
 
 fn update_primitives(
     mut commands: Commands,
-    mut primitives: Query<(Entity, &Primitive, Option<&mut Handle<Mesh>>), Changed<Primitive>>,
+    mut primitives: Query<
+        (
+            Entity,
+            &Primitive,
+            &Transform,
+            Option<&mut Handle<Mesh>>,
+            Added<Primitive>,
+        ),
+        Changed<Primitive>,
+    >,
     mut material: Query<&DebugMaterial>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
@@ -208,36 +216,48 @@ fn update_primitives(
     }
     let material = material.unwrap();
 
-    for (entity, primitive, mesh_handle) in primitives.iter() {
-        let new_mesh:  Mesh = match &*primitive.shape {
+    for (entity, primitive, transform, mesh_handle, is_added) in primitives.iter() {
+        println!("{:?}", transform);
+        let new_mesh: Mesh = match &*primitive.shape {
             "cube" => shape::Cube::default().into(),
             "box" => shape::Box::default().into(),
             "capsule" => shape::Capsule::default().into(),
             "torus" => shape::Torus::default().into(),
             "icosphere" => shape::Icosphere::default().into(),
             "uvsphere" => shape::UVSphere::default().into(),
-            _ => panic!("Unknown Primitive shape {}", primitive.shape)
+            _ => panic!("Unknown Primitive shape {}", primitive.shape),
         };
 
         let mesh_handle = if let Some(mesh_handle) = mesh_handle {
             *meshes.get_mut(mesh_handle).unwrap() = new_mesh;
             mesh_handle.clone()
-        }
-        else {
+        } else {
             let handle = meshes.add(new_mesh);
             commands.entity(entity).insert(handle.clone());
             handle
         };
 
-        commands
-            .spawn_bundle(PbrBundle {
-                mesh: mesh_handle,
-                material: material.handle.clone(),
-                ..default()
-            })
-            .insert(Shape);
+        if is_added {
+            commands
+                .entity(entity)
+                // .insert_bundle(PbrBundle {
+                //     mesh: mesh_handle,
+                //     material: material.handle.clone(),
+                //     transform: transform.clone()    ,
+                //     ..default()
+                // })
+                .insert_bundle((
+                    mesh_handle,
+                    material.handle.clone(),
+                    GlobalTransform::default(),
+                    Visibility::default(),
+                    ComputedVisibility::default(),
+                ))
+                .insert(Shape);
+        } else {
+            // commands.entity(entity).insert(mesh_handle);
+        }
     }
-
 }
 
 fn rotate(mut query: Query<&mut Transform, With<Shape>>, time: Res<Time>) {
@@ -272,4 +292,27 @@ fn uv_debug_texture() -> Image {
         &texture_data,
         TextureFormat::Rgba8UnormSrgb,
     )
+}
+
+fn inspect(
+    keyboard: Res<Input<KeyCode>>,
+    all_entities: Query<Entity>,
+    entities: &Entities,
+    archetypes: &Archetypes,
+    components: &Components,
+) {
+    if keyboard.just_pressed(KeyCode::Space) {
+        for entity in all_entities.iter() {
+            println!("Entity: {:?}", entity);
+            if let Some(entity_location) = entities.get(entity) {
+                if let Some(archetype) = archetypes.get(entity_location.archetype_id) {
+                    for component in archetype.components() {
+                        if let Some(info) = components.get_info(component) {
+                            println!("\tComponent: {}", info.name());
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
