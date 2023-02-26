38f0e117b8d67118a2fd2976156e0c2b14de7e43
Polgár András <azazdeaz@gmail.com>
2023-02-26 19:05:21 +0100

    Make object pushable

--- crates/push_test/src/main.rs
+++ crates/push_test/src/main.rs
@@ -14,7 +14,13 @@ fn main() {
         .add_plugins(bevy_mod_picking::DefaultPickingPlugins)
         .add_plugin(bevy_transform_gizmo::TransformGizmoPlugin::default())
         .add_startup_system(setup)
+        .add_system(handle_collisions)
         .run();
+}
+
+#[derive(Component)]
+struct Pushable {
+    home_position: Transform,
 }
 
 fn setup(
@@ -49,6 +55,22 @@ fn setup(
     //     ..default()
     // });
 
+    let transform = Transform::from_xyz(-2.0, 1.0, 0.0);
+    commands
+        .spawn(RigidBody::KinematicPositionBased)
+        // .insert(Collider::ball(0.3))
+        .insert(Collider::ball(0.4))
+        // .insert(PbrBundle {
+        //     mesh: meshes.add(Mesh::from(shape::Box::new(0.2, 0.2, 4.0))),
+        //     material: materials.add(Color::rgb(0.5, 0.3, 0.3).into()),
+        //     ..default()
+        // })
+        .insert(Restitution::coefficient(0.7))
+        .insert(Pushable {
+            home_position: transform,
+        })
+        .insert(TransformBundle::from(transform));
+
     /* Create the bouncing ball. */
     commands
         .spawn(RigidBody::KinematicVelocityBased)
@@ -64,5 +86,70 @@ fn setup(
         .insert_bundle(bevy_mod_picking::PickableBundle::default())
         .insert(bevy_transform_gizmo::GizmoTransformable)
         .insert(TransformBundle::from(Transform::from_xyz(-2.0, 2.0, 0.0)));
+}
 
+fn handle_collisions(
+    mut pushables: Query<(Entity, &mut GlobalTransform), With<Pushable>>,
+    rapier_context: Res<RapierContext>,
+    mut colliders_query: Query<
+        (&Collider, &GlobalTransform, Option<&mut Velocity>),
+        Without<Pushable>,
+    >,
+    time: Res<Time>,
+) {
+    let delta_time = time.delta_seconds();
+    for (entity, mut transform) in pushables.iter_mut() {
+        let collider_offset = 0.4;
+        let collider_velocity_coefficient = 1.0;
+        for contact_pair in rapier_context.contacts_with(entity) {
+            let other_entity = if contact_pair.collider1() == entity {
+                contact_pair.collider2()
+            } else {
+                contact_pair.collider1()
+            };
+            let Ok((other_collider, other_transform, other_velocity)) = colliders_query.get_mut(other_entity) else {
+                // error!("Couldn't find collider on entity {:?}", entity);
+                continue;
+            };
+            let vel = other_velocity.as_ref().map_or(0.0, |v| {
+                v.linvel.length_squared() * delta_time * delta_time * collider_velocity_coefficient
+            });
+            let pushed_position = {
+                let other_transform = other_transform.compute_transform();
+                let transform = transform.compute_transform();
+                let projected_point = other_collider.project_point(
+                    other_transform.translation,
+                    other_transform.rotation,
+                    transform.translation,
+                    false,
+                );
+
+                let normal: Vec3 = (projected_point.point - transform.translation)
+                    .try_normalize()
+                    .unwrap_or(Vec3::Y);
+                if projected_point.is_inside {
+                    Some(projected_point.point + (normal * collider_offset) + (normal * vel))
+                } else if transform
+                    .translation
+                    .distance_squared(projected_point.point)
+                    < collider_offset * collider_offset
+                {
+                    Some(projected_point.point - (normal * collider_offset))
+                } else {
+                    None
+                }
+            };
+            if let Some(position) = pushed_position {
+                *transform.translation_mut() = position.into();
+            }
+
+            // let collider_dampen_others = Some(0.05);
+            // if let Some((ref mut vel, dampen_coef)) = other_velocity.zip(collider_dampen_others) {
+            //     let damp = 1.0 - dampen_coef;
+            //     vel.linvel *= damp;
+            //     vel.angvel *= damp;
+            // }
+        }
+        // *rapier_collider = get_collider(rendering, collider, None);
+    }
 }
