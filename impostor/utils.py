b7bd3a68d3a80484bf25161108621f03b0d7435c
Polgar Andras <azazdeaz@gmail.com>
2025-01-05 13:13:11 +0900

    Add initial Curve implementation

--- impostor/utils.py
+++ impostor/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from dataclasses import dataclass, replace
+from scipy.interpolate import CubicSpline
 
 @dataclass
 class NormalDistribution:
@@ -26,3 +27,27 @@ def fluent_methods(cls):
             lambda self, val, field=field: replace(self, **{field: val}),
         )
     return cls
+
+class Curve:
+    def __init__(self, control_points: list[tuple[float, float]]):
+        """
+        Initialize the curve with control points.
+
+        Args:
+        control_points (list[tuple[float, float]]): The control points of the curve.
+        """
+        self._control_points = control_points
+        self.curve = CubicSpline(*zip(*self._control_points), bc_type="not-a-knot")
+        
+
+    def evaluate(self, t):
+        """
+        Evaluate the curve at t.
+
+        Args:
+        t (float): The parameter value at which to evaluate the curve.
+
+        Returns:
+        tuple[float, float]: The point on the curve at t.
+        """
+        return self.curve(t)
