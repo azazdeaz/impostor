3619a410967e6bffc5db35e0531507de463b2edb
Andras Polgar <azazdeaz@gmail.com>
2025-07-26 11:46:39 +0200

    reset again. going back to rust....

--- src/impostor_gen/vec3.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-class Vec3:
-    def __init__(self, x: float = 0.0, y: float = 0.0, z: float = 0.0):
-        self.xyz = np.array([x, y, z], dtype=float)
-    
-    def __add__(self, other: 'Vec3' | float):
-        return Vec3(*(self.xyz + other.xyz))
